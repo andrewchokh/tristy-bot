940e38f09d2e851f33bcdd80bbd54e53cdbb58c0
Andrew <88233242+andrewchokh@users.noreply.github.com>
2022-04-15 12:20:56 +0300

    Update purge.ts

--- commands/purge.ts
+++ commands/purge.ts
@@ -8,7 +8,7 @@ export = {
 	permissions: ["MANAGE_MESSAGES"],
 	aliases: ['clear'],
 
-	run: async (client: DiscordBot, message: Message, args: any) => {
+	run: async (client: DiscordBot, message: Message, args: string[]) => {
 		const amount = parseInt(args[0]);
 
         if (!amount) return message.channel.send("Please specify the amount of messages you want me to delete");
@@ -39,9 +39,8 @@ export = {
             }
         ],
 
-		run: async (client: DiscordBot, interaction: CommandInteraction, args: any) => {
+		run: async (client: DiscordBot, interaction: CommandInteraction, args: string[]) => {
 			const amount = interaction.options.getInteger('amount');   
-
 
             if (!amount) return interaction.reply("Please specify the amount of messages you want me to delete");
             if (amount > 100 || amount < 1) return interaction.reply("Please select a number *between* 100 and 1");
