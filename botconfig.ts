4aba6ab2224339e198fd7b760beaca381d6391d7
Andrew <88233242+andrewchokh@users.noreply.github.com>
2022-04-15 11:20:41 +0300

    Fix botConfig.ts

--- botconfig.ts
+++ botconfig.ts
@@ -7,10 +7,10 @@ export = {
     clientId: "832665697900560416", // Discord client ID 
     token: process.env.TOKEN || null, // Bot's token
     mongoUri: process.env.MONGO_URI || null, // URI link to MongoDB
-    intents: process.env.INTENTS || null, // Intents for the bot
-    defaultPrefix: process.env.PREFIX || ">", // Default prefix for bot commands
-    defaultLanguage: process.env.LANGUAGE || "english", // Default language of bot
-    testingServerId: process.env.TESTING_SERVER_ID || null, // Server for test of slash commands
+    intents: 131071, // Intents for the bot
+    defaultPrefix: ">", // Default prefix for bot commands
+    defaultLanguage: "english", // Default language of bot
+    testingServerId: "831960677949505556", // Server for test of slash commands
     presence: {
         status: "online", // online, idle, dnd
         name: ">help", // The message shown
