59ffe966601a712836212423ded5e750572625bd
Andrew <88233242+andrewchokh@users.noreply.github.com>
2022-04-15 12:20:46 +0300

    Update botconfig.ts

--- botconfig.ts
+++ botconfig.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export = {
-    ownerId: "538494819664789544", // ID of the bot owner
-    clientId: "832665697900560416", // Discord client ID 
+    ownerId: '538494819664789544', // ID of the bot owner
+    clientId: '832665697900560416', // Discord client ID 
     token: process.env.TOKEN || null, // Bot's token
     mongoUri: process.env.MONGO_URI || null, // URI link to MongoDB
     intents: 131071, // Intents for the bot
-    defaultPrefix: ">", // Default prefix for bot commands
-    defaultLanguage: "english", // Default language of bot
-    testingServerId: "831960677949505556", // Server for test of slash commands
+    defaultPrefix: '>', // Default prefix for bot commands
+    defaultLanguage: 'english', // Default language of bot
+    testingServerId: '831960677949505556', // Server for test of slash commands
     presence: {
-        status: "online", // online, idle, dnd
-        name: ">help", // The message shown
-        type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING
+        status: 'online', // online, idle, dnd
+        name: '>help', // The message shown
+        type: 'WATCHING', // PLAYING, WATCHING, LISTENING, STREAMING
     },
 }
