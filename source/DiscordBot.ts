cd39906da7904291b3d6bef557d5f1a97e46087f
Andrew <88233242+andrewchokh@users.noreply.github.com>
2022-04-15 11:41:57 +0300

    Update DiscordBot.ts

--- source/DiscordBot.ts
+++ source/DiscordBot.ts
@@ -22,6 +22,7 @@ class DiscordBot extends Client {
         this.loadCommands();
         this.loadEvents();
 
+        
         this.registerCommandsInDB();
     }  
 
@@ -77,7 +78,7 @@ class DiscordBot extends Client {
     }
 
     registerSlashCommands() {
-        require('../utils/registerSlashCommands')(this, this.guilds.cache);
+        require('../utils/registerSlashCommands')(this);
     }
 
     registerCommandsInDB() {
