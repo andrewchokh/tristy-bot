33a15666e478a7a657b24028c57ad8ad9d8a4d07
Andrew <88233242+andrewchokh@users.noreply.github.com>
2022-04-15 11:42:39 +0300

    Delete guildDelete.ts

--- events/guildDelete.ts
+++
@@ -1,8 +0,0 @@
-import guildsModel from '../models/guild';
-
-export = async (client: any, guild: any) => {
-    await guildsModel.deleteOne({id: String(guild.id)})
-    .then(() => {
-        client.logger.log("Bot has left to the server: " + guild.name);
-    });
-}
